b708fc02021251cfc1f9fb976ca8fe27a3aa82fd
ndSpacehead <damh17@gmail.com>
2024-07-28 23:24:11 +0300

    add none-ls

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+                -- other look in BUILTINS.md: https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+				--null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.completion.spell,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
