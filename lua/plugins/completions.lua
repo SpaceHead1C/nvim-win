b121f5bd2ac818de6dc051e9ffc1a82ed2ffd19d
ndSpacehead <damh17@gmail.com>
2024-08-01 20:34:09 +0300

    improve plugins configs

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -11,10 +11,18 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"onsails/lspkind.nvim",
+		},
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
+			local lspkind = require("lspkind")
 			local cmp = require("cmp")
 			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
+				},
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
@@ -25,6 +33,8 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
@@ -34,9 +44,15 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-				}, {
 					{ name = "buffer" },
+					{ name = "path" },
 				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
 			})
 		end,
 	},
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
-	"nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			--ensure_installed = { "html", "lua", "vim", "vimdoc" }, -- see other on https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-			auto_install = true,
-			highlight = { enable = true },
-			ident = { enable = true },
-		})
-	end,
+    "nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            --ensure_installed = { "html", "lua", "vim", "vimdoc" }, -- see other on https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+            auto_install = true,
+            highlight = { enable = true },
+            ident = { enable = true },
+            autotag = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-s>",
+                    node_incremental = "<C-s>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
 }
